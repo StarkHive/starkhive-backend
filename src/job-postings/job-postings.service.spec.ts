d00399780a90dd6daca40eeb42af68485ddb4495
Omachonu Prosper <omachonucodes@gmail.com>
2025-03-15 04:11:22 +0100

    Create mock methods for test suites

--- src/job-postings/job-postings.service.spec.ts
+++ src/job-postings/job-postings.service.spec.ts
@@ -4,28 +4,53 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JobPosting } from './entities/job-posting.entity';
 
+// Create a mock for Repository
+class MockJobPostingRepository {
+  create = jest.fn().mockReturnValue(true);
+  save = jest.fn().mockResolvedValue({
+    id: 1,
+    title: 'NestJS Dev',
+    description: 'Build APIs',
+    company: 'Tech Co',
+  });
+}
+
 describe('JobPostingsService', () => {
   let service: JobPostingsService;
-  let repository: Repository<JobPosting>;
+  let repository: MockJobPostingRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         JobPostingsService,
         {
-          provide: getRepositoryToken(JobPosting), // Mock the repository
-          useClass: Repository, // Use a TypeORM repository mock
+          provide: getRepositoryToken(JobPosting),
+          useClass: MockJobPostingRepository, // Use the mock repository
         },
       ],
     }).compile();
 
     service = module.get<JobPostingsService>(JobPostingsService);
-    repository = module.get<Repository<JobPosting>>(
+    repository = module.get<MockJobPostingRepository>(
       getRepositoryToken(JobPosting),
     );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a job', async () => {
+    const job = await service.create({
+      title: 'NestJS Dev',
+      description: 'Build APIs',
+      company: 'Tech Co',
+    });
+    expect(job).toHaveProperty('id');
+    expect(repository.create).toHaveBeenCalledWith({
+      title: 'NestJS Dev',
+      description: 'Build APIs',
+      company: 'Tech Co',
+    });
   });
 });
